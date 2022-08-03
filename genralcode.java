9dc0f429b98316b209f128510cb109c434fbf519
Akshay4401 <akshayclk1@gmail.com>
2022-08-03 10:12:12 +0530

    genralcode is added

--- genralcode.java
+++ genralcode.java
@@ -12,14 +12,16 @@ import org.testng.annotations.BeforeTest;
 public class genralcode {
 		WebDriver driver;
 		@BeforeMethod
-		public void launch()
+		public void launch() throws InterruptedException
 		{
 			System.setProperty("webdriver.chrome.driver","C:\\Users\\akshay.dm\\Downloads\\chromedriver_win32\\chromedriver.exe");
-			ChromeDriver driver =new ChromeDriver();
+			 driver =new ChromeDriver();
 			 driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 			driver.get("https://mobileworld.azurewebsites.net/");
 			driver.manage().window().maximize();
 		}
+		
+		
 		@AfterMethod
 		public void close()
 		{
